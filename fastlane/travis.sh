59394a09d3664df13134717fbbbe4c62950e008c
Jason Duffy <jasonsduffy@gmail.com>
2016-04-20 23:58:14 -0400

    trying to use cert fastlane tool

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -6,9 +6,6 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
 fi
 
 if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
-    # Travis fetches a shallow clone. We use commit count until HEAD for build number. In order to assure that the count is correct we have to `unshallow` Travis' clone.
-  git fetch --unshallow
-
   fastlane beta
   exit $?
 fi
