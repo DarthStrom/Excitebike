fbbe3d22188e299f9813ffecfc6b2746c142d8c2
Jason Duffy <jasonsduffy@gmail.com>
2016-04-26 22:38:31 -0400

    skip beta build from ci for now

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -7,7 +7,10 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
     exit $?
 fi
 
-if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
-  fastlane beta
-  exit $?
-fi
+# not working yet, can't pull the certificates repo
+# this needs to be done locally for now
+#
+# if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+#   fastlane beta
+#   exit $?
+# fi
