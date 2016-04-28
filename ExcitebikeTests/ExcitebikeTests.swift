5a4697a08f19105506e84c98c87d2cfb0af891ac
Jason Duffy <jasonsduffy@gmail.com>
2016-04-27 22:59:28 -0400

    test coverage for what we have, nav to new scene

--- ExcitebikeTests/ExcitebikeTests.swift
+++ ExcitebikeTests/ExcitebikeTests.swift
@@ -6,31 +6,56 @@
 //  Copyright © 2016 Pairing Group. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
+import SpriteKit
+
 @testable import Excitebike
 
-class ExcitebikeTests: XCTestCase {
+class ExcitebikeTests: QuickSpec {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    override func spec() {
+
+        var subject: GameScene!
+
+        beforeEach {
+            subject = GameScene(size: CGSize(width: 100, height: 200))
+        }
+
+        describe("didMoveToView") {
+
+            it("makes a label") {
+
+                subject.didMoveToView(SKView())
+
+                let label = subject.childNodeWithName("Title") as? SKLabelNode
+                expect(label?.fontName).to(equal("Marker Felt"))
+                expect(label?.text).to(equal("Excite Bike!"))
+                expect(label?.fontSize).to(equal(45))
+                expect(label?.position).to(equal(CGPoint(x: CGRectGetMidX(subject.frame), y: CGRectGetMidY(subject.frame))))
+            }
+        }
+
+        describe("touchesBegan") {
+
+            it("starts the game") {
+                let mockView = MockView()
+                mockView.presentScene(subject)
+
+                subject.touchesBegan([], withEvent: nil)
+
+                expect(subject.view).to(beIdenticalTo(mockView))
+                expect(mockView.mocker.getInvocationCountFor("presentScene")).to(equal(1))
+                // TODO: check that it shows the right scene
+            }
         }
     }
-    
+
+    private class MockView: SKView {
+        var mocker = Mocker()
+
+        override func presentScene(scene: SKScene, transition: SKTransition) {
+            mocker.recordInvocation("presentScene", paramList: [scene, transition])
+        }
+    }
 }
---
+++ ExcitebikeTests/GameViewControllerTests.swift
@@ -0,0 +1,73 @@
+//
+//  GameViewControllerTests.swift
+//  Excitebike
+//
+//  Created by Jason Duffy on 4/27/16.
+//  Copyright © 2016 Pairing Group. All rights reserved.
+//
+
+import Quick
+import Nimble
+import SpriteKit
+
+@testable import Excitebike
+
+class GameViewControllerTests: QuickSpec {
+
+    override func spec() {
+
+        describe("GameViewController") {
+
+            var subject: GameViewController!
+
+            beforeEach {
+                subject = UIStoryboard.init(name: "Main", bundle: NSBundle(identifier: "com.pairinggroup.Excitebike.development")).instantiateViewControllerWithIdentifier("TitleViewController") as? GameViewController
+            }
+
+            describe("viewDidLoad") {
+
+                it("shows the title screen") {
+                    let mockView = MockView()
+                    subject.view = mockView
+
+                    subject.viewDidLoad()
+
+                    expect(subject.view).to(beIdenticalTo(mockView))
+                    expect(mockView.mocker.getInvocationCountFor("presentScene")).to(equal(1))
+                    let scene = mockView.mocker.getParametersFor("presentScene")?[0] as? GameScene
+                    expect(scene?.size).to(equal(mockView.bounds.size))
+                    expect(scene?.scaleMode).to(equal(SKSceneScaleMode.AspectFit))
+                }
+            }
+
+            describe("shouldAutorotate") {
+
+                it("is true") {
+                    expect(subject.shouldAutorotate()).to(beTrue())
+                }
+            }
+
+            describe("supportedInterfaceOrientations") {
+
+                it("supports all but upside down if device is a phone") {
+                    expect(subject.supportedInterfaceOrientations()).to(equal(UIInterfaceOrientationMask.AllButUpsideDown))
+                }
+            }
+
+            describe("prefersStatusBarHidden") {
+
+                it("is true") {
+                    expect(subject.prefersStatusBarHidden()).to(beTrue())
+                }
+            }
+        }
+    }
+
+    private class MockView: SKView {
+        var mocker = Mocker()
+
+        override func presentScene(scene: SKScene?) {
+            mocker.recordInvocation("presentScene", paramList: [scene!])
+        }
+    }
+}
